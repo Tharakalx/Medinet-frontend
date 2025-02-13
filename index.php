90d9367ab70abe1761eaa5f90a2b05ede332f796
Tharaka Lakshan <126232613+Tharakalx@users.noreply.github.com>
2025-02-13 11:29:23 +0530

    upload files

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php include 'header.php'; ?>
+<main>
+    <section class="main1">
+        <h1>Welcome to MediNet</h1> 
+        <button onclick="showMessage()">click</button>
+
+    </section>
+</main>
+<?php include 'header.php'; ?>
