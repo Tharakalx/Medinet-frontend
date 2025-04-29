941480bb62258239411f2a1512a9d02a343e0bac
ksdfernando <ksachithradf@gmail.com>
2025-04-29 11:37:44 +0530

    commit new 2

--- includes/header.php
+++ includes/header.php
@@ -15,7 +15,7 @@
                 <li><a href="index.php">Home</a></li>
                 <li><a href="pages/register.php">register</a></li>
                 <li><a href="#">Contact</a></li>
-                <li><a href="index.php#login">Login</a></li>
+                <li><a href="pages/login.php">Login</a></li>
 
             </ul>
         </nav>
